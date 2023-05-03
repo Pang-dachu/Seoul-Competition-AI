a4e6694f83398eb0b0a800b7b31bf6d67beb2602
Kim Yoo <kimyoo04eco@naver.com>
2023-05-03 20:58:26 +0900

    feat: recommend 폴더 함수 api에 적용

--- routers/recommend.py
+++ routers/recommend.py
@@ -1,22 +1,27 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from recommend import model_update, user_recommend
 
 
-class TextData(BaseModel):
-    text: str
+class TSearchKeyword(BaseModel):
+    searchKeyword: str
 
 router = APIRouter()
 
+'''
+- 일주일에 한번 응답
+- TfidfVectorizer 모델 /server 에 저장
+- DateFrame /server 에 저장
+'''
+@router.get("/recreate")
+def my_route():
+    model_update.model_update()
+    return {"message": "success"}
 
-@router.get("/my-route")
-async def my_route():
-    return {"message": "Hello, World!"}
-
-
-def recommendation(text):
-    return text
-
+"""
+- searchKeyword와 함께 POST 요청 받음
+"""
 @router.post("/predict")
-def predict(text_data: TextData):
-    prediction = recommendation(text_data.text)
+def predict(data: TSearchKeyword):
+    prediction = user_recommend.edu_recommend(data.searchKeyword)
     return {"prediction": prediction}
