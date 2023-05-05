54228485a40716f9c33095802e17e6e8819329ce
Kim Yoo <kimyoo04eco@naver.com>
2023-05-05 15:03:15 +0900

    fix: -mysql-connector-python 대신 pymysql 설치

--- db/__init__.py
+++ db/__init__.py
@@ -1,11 +1,18 @@
-import mysql.connector
-
+import pymysql
+import pymysql.cursors
 
 async def get_mysql_connection():
-    conn = mysql.connector.connect(
-        host="mysqldb",
-        user="test_user",
-        password="a8Gh@c8wi#gL^",
-        database="userdb"
-    )
-    return conn
+    config = {
+        "host":"mysqldb",
+        "user":"test_user",
+        "password":"a8Gh@c8wi#gL^",
+        "database":"userdb",
+        "charset":'utf8mb4',
+        "connect_timeout":30,
+        "read_timeout":30,
+        "write_timeout":30,
+        "cursorclass": pymysql.cursors.DictCursor  # 딕셔너리 형태로 결과를 반환하도록 설정
+    }
+
+    connection = pymysql.connect(**config)
+    return connection
