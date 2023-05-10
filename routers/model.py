9a4bfbd23d597f4e0947aebf876e87d975868d0d
팡다츄 <fox3931@gmail.com>
2023-05-10 22:22:49 +0900

    feat: 모델 업데이트 라우터 초기 설정중

---
+++ routers/model.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
+
+router = APIRouter()
+
+# 요기 수정하기 
+# json으로 받기 
+
+class TEducations(BaseModel):
+    id: int
+    name: str
+    status: str
+    price: str
+    capacity: int
+    registerStart: str
+    registerEnd: str
+    educationStart: str
+    educationEnd: str
+    url: str
+    hits: int
+    reviewsCount: dict[]
+
+
+class TchatHistories(BaseModel):
+    id: int 
+    question: str
+    answer: str
+    feedback: bool
+    createdAt: datetime
+
+
+class TModelUpdateData(BaseModel):
+    educations: TEducations[]
+    chatHistories: TchatHistories[]
+
+
+@router.post("/")
+def predict(data: TModelUpdateData):
+    
+    # answer = 문장나오는함수(data.question)
+    # return {"answer": answer}
+
+    return {"answer": 0}
